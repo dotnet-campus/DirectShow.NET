2b28e8ca6e7189ffa988b39e9a5e1dd7f5431fd2
kawaic <kawaic@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-05-16 22:40:28 +0000

    starting ienumpinstest
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@85 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.0/IEnumPinsTest.cs
@@ -0,0 +1,15 @@
+// $Id: IEnumPinsTest.cs,v 1.1 2005-05-16 22:40:28 kawaic Exp $
+// $Author: kawaic $
+// $Revision: 1.1 $
+using NUnit.Framework;
+
+namespace DirectShowLib.Test
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class IEnumPinsTest
+	{
+	}
+}
