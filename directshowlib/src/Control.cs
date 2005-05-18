85b0ff0a5ce0fb15b545cff2791f74e469711421
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-05-18 09:22:11 +0000

    move IBasicAudio outside the ALLOW_UNTESTED_INTERFACES block
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@86 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/src/Control.cs
+++ directshowlib/src/Control.cs
@@ -349,24 +349,6 @@ namespace DirectShowLib
 	}
 
 	[ComVisible(true), ComImport,
-		Guid("56a868b3-0ad4-11ce-b03a-0020af0ba770"),
-		InterfaceType(ComInterfaceType.InterfaceIsDual)]
-	public interface IBasicAudio
-	{
-		[PreserveSig]
-		int put_Volume([In] int lVolume);
-
-		[PreserveSig]
-		int get_Volume([Out] out int plVolume);
-
-		[PreserveSig]
-		int put_Balance([In] int lBalance);
-
-		[PreserveSig]
-		int get_Balance([Out] out int plBalance);
-	}
-
-	[ComVisible(true), ComImport,
 		Guid("56a868b5-0ad4-11ce-b03a-0020af0ba770"),
 		InterfaceType(ComInterfaceType.InterfaceIsDual)]
 	public interface IBasicVideo
@@ -996,5 +978,23 @@ namespace DirectShowLib
 		int IsCursorHidden([Out] out OABool hideCursor);
 	}
 
-	#endregion
+  [ComVisible(true), ComImport,
+  Guid("56a868b3-0ad4-11ce-b03a-0020af0ba770"),
+  InterfaceType(ComInterfaceType.InterfaceIsDual)]
+  public interface IBasicAudio
+  {
+    [PreserveSig]
+    int put_Volume([In] int lVolume);
+
+    [PreserveSig]
+    int get_Volume([Out] out int plVolume);
+
+    [PreserveSig]
+    int put_Balance([In] int lBalance);
+
+    [PreserveSig]
+    int get_Balance([Out] out int plBalance);
+  }
+
+  #endregion
 }
