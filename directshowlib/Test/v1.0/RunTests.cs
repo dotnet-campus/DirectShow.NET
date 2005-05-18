e3ede435dfa56a60ddfd97a5e91d7247982e64ee
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-05-18 12:12:44 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@93 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Test/v1.0/RunTests.cs
+++ directshowlib/Test/v1.0/RunTests.cs
@@ -51,17 +51,20 @@ namespace DirectShowLib.Test
 //      IVMRWindowlessControlTest t11 = new IVMRWindowlessControlTest();
 //      t11.DoTests();
 
-      IPersistTest t12 = new IPersistTest();
-      t12.DoTests();
+      //IPersistTest t12 = new IPersistTest();
+      //t12.DoTests();
 
-      IMediaFilterTest t13 = new IMediaFilterTest();
-      t13.DoTests();
+      //IMediaFilterTest t13 = new IMediaFilterTest();
+      //t13.DoTests();
 
-      IBaseFilterTest t14 = new IBaseFilterTest();
-      t14.DoTests();
+      //IBaseFilterTest t14 = new IBaseFilterTest();
+      //t14.DoTests();
 
-      IBasicAudioTest t15 = new IBasicAudioTest();
-      t15.DoTests();
+      //IBasicAudioTest t15 = new IBasicAudioTest();
+      //t15.DoTests();
+
+      IFilterGraphTest t16 = new IFilterGraphTest();
+      t16.DoTests();
   
     }
 	}
