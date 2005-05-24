70d8a7d91aff483c9e87c8f605d42461ce9bc33e
kawaic <kawaic@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-05-24 17:29:50 +0000

    adding new enum media type test
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@108 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.0/IEnumMediaTypesTest.cs
@@ -0,0 +1,46 @@
+// $Id: IEnumMediaTypesTest.cs,v 1.1 2005-05-24 17:29:50 kawaic Exp $
+// $Author: kawaic $
+// $Revision: 1.1 $
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+namespace DirectShowLib.Test
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[TestFixture]
+	public class IEnumPinsTest
+	{
+		[Test]
+		public void TestNext()
+		{
+
+		}
+
+		[Test]
+		public void TestReset()
+		{
+
+		}
+
+		[Test]
+		public void TestSkip()
+		{
+
+		}
+
+		[Test]
+		public void TestClone()
+		{
+
+		}
+
+
+		private IEnumPins GetTestEnum()
+		{
+			
+		}
+
+	}
+}
