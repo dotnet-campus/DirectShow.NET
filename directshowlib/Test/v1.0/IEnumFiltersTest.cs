8aa6b7045e9b3b8f296fdc922a6977c3d791005c
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-06-02 13:14:22 +0000

    IEnumFilters test framework
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@170 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.0/IEnumFiltersTest.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DirectShowLib.Test
+{
+	public class IEnumFiltersTest
+	{
+    IGraphBuilder graphBuilder = null;
+    IEnumFilters enumFilters = null;
+    
+    public IEnumFiltersTest()
+		{
+		}
+
+    public void DoTests()
+    {
+      BuildGraph(@"..\..\..\Resources\foo.avi");
+      
+      try
+      {
+        TestClone();
+        TestNext();
+        TestReset();
+        TestSkip();
+      }
+      finally
+      {
+        while(Marshal.ReleaseComObject(this.enumFilters) > 0);
+        while(Marshal.ReleaseComObject(this.graphBuilder) > 0);
+      }
+    }
+
+    public void TestClone()
+    {
+      int hr = 0;
+      IEnumFilters clone = null;
+      IBaseFilter[] filters = new IBaseFilter[1];
+      int fetched = 0;
+      int filterCount1 = 0;
+      int filterCount2 = 0;
+
+      // Reset enumeration to be sure we are at the begining
+      hr = this.enumFilters.Reset();
+      DsError.ThrowExceptionForHR(hr);
+      
+      // Clone our enumeration object
+      hr = this.enumFilters.Clone(out clone);
+      DsError.ThrowExceptionForHR(hr);
+
+      // Count how many filters are in this enumearation
+      while(this.enumFilters.Next(1, filters, out fetched) == 0)
+      {
+        Marshal.ReleaseComObject(filters[0]);
+        filterCount1++;
+      }
+
+      // Count how many filters are in this enumearation
+      while(clone.Next(1, filters, out fetched) == 0)
+      {
+        Marshal.ReleaseComObject(filters[0]);
+        filterCount2++;
+      }
+
+      // both enumeration should have equal items number
+      // 6 filters for me
+      Debug.Assert(filterCount1 == filterCount2, "IEnumFilters.Clone");
+    }
+
+    public void TestNext()
+    {
+      int hr = 0;
+      IBaseFilter[] filters = new IBaseFilter[1];
+      int fetched = 0;
+      FilterInfo fi = new FilterInfo();
+
+      // Reset enumeration to be sure we are at the begining
+      hr = this.enumFilters.Reset();
+      DsError.ThrowExceptionForHR(hr);
+
+      // Get the first filter
+      hr = this.enumFilters.Next(1, filters, out fetched);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = filters[0].QueryFilterInfo(out fi);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(fi.achName.Equals("Default DirectSound Device"), "IEnumFilters.Next");
+      Marshal.ReleaseComObject(fi.pGraph);
+
+      // Get the second filter
+      hr = this.enumFilters.Next(1, filters, out fetched);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = filters[0].QueryFilterInfo(out fi);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(fi.achName.Equals("Video Renderer"), "IEnumFilters.Next");
+      Marshal.ReleaseComObject(fi.pGraph);
+    }
+
+    public void TestReset()
+    {
+      int hr = 0;
+
+      hr = this.enumFilters.Reset();
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(hr == 0, "IEnumFilters.Reset");
+    }
+
+    public void TestSkip()
+    {
+      int hr = 0;
+      IBaseFilter[] filters = new IBaseFilter[1];
+      int fetched = 0;
+      int filterCount1 = 0;
+      int filterCount2 = 0;
+
+      // Count how many filters are in this enumearation
+      while(this.enumFilters.Next(1, filters, out fetched) == 0)
+      {
+        Marshal.ReleaseComObject(filters[0]);
+        filterCount1++;
+      }
+
+      hr = this.enumFilters.Reset();
+      DsError.ThrowExceptionForHR(hr);
+
+      // Count how many filters are in this enumearation but skip on each time
+      while(this.enumFilters.Next(1, filters, out fetched) == 0)
+      {
+        // skip one filter
+        hr = this.enumFilters.Skip(1);
+        DsError.ThrowExceptionForHR(hr);
+
+        Marshal.ReleaseComObject(filters[0]);
+        filterCount2++;
+      }
+
+      // second count shhould be half the first
+      Debug.Assert(filterCount1 == filterCount2 * 2, "IEnumFilters.Skip");
+    }
+
+    private void BuildGraph(string fileName)
+    {
+      int hr = 0;
+      this.graphBuilder = (IGraphBuilder) new FilterGraph();
+
+      hr = this.graphBuilder.RenderFile(fileName, null);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = this.graphBuilder.EnumFilters(out this.enumFilters);
+      DsError.ThrowExceptionForHR(hr);
+    }
+	}
+}
