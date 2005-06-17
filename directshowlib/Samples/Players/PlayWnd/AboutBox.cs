aac1dacf4a327e4120d999d82421dcbe1fa62d89
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-06-17 21:25:28 +0000

    Fix about box to use new licensing
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@277 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Samples/Players/PlayWnd/AboutBox.cs
+++ directshowlib/Samples/Players/PlayWnd/AboutBox.cs
@@ -45,77 +45,77 @@ namespace DirectShowLib.Sample
 		/// </summary>
 		private void InitializeComponent()
 		{
-      this.label1 = new System.Windows.Forms.Label();
-      this.button1 = new System.Windows.Forms.Button();
-      this.label2 = new System.Windows.Forms.Label();
-      this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-      this.label3 = new System.Windows.Forms.Label();
-      this.SuspendLayout();
-      // 
-      // label1
-      // 
-      this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.label1.Location = new System.Drawing.Point(8, 8);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(272, 40);
-      this.label1.TabIndex = 1;
-      // 
-      // button1
-      // 
-      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.button1.Location = new System.Drawing.Point(288, 8);
-      this.button1.Name = "button1";
-      this.button1.TabIndex = 0;
-      this.button1.Text = "OK";
-      this.button1.Click += new System.EventHandler(this.button1_Click);
-      // 
-      // label2
-      // 
-      this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.label2.Location = new System.Drawing.Point(8, 64);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(128, 16);
-      this.label2.TabIndex = 2;
-      this.label2.Text = "Source code release under";
-      // 
-      // linkLabel1
-      // 
-      this.linkLabel1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.linkLabel1.Location = new System.Drawing.Point(144, 64);
-      this.linkLabel1.Name = "linkLabel1";
-      this.linkLabel1.Size = new System.Drawing.Size(200, 16);
-      this.linkLabel1.TabIndex = 3;
-      this.linkLabel1.TabStop = true;
-      this.linkLabel1.Text = "Apache Software License Version 1.1";
-      this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-      // 
-      // label3
-      // 
-      this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.label3.Location = new System.Drawing.Point(8, 48);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(320, 16);
-      this.label3.TabIndex = 4;
-      this.label3.Text = "DirectShowLib Version : ";
-      // 
-      // AboutBoxWnd
-      // 
-      this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-      this.ClientSize = new System.Drawing.Size(372, 86);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.linkLabel1);
-      this.Controls.Add(this.label2);
-      this.Controls.Add(this.button1);
-      this.Controls.Add(this.label1);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-      this.MaximizeBox = false;
-      this.MinimizeBox = false;
-      this.Name = "AboutBoxWnd";
-      this.ShowInTaskbar = false;
-      this.Text = "AboutBox";
-      this.ResumeLayout(false);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 40);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(288, 8);
+            this.button1.Name = "button1";
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Location = new System.Drawing.Point(8, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Source code release under";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.linkLabel1.Location = new System.Drawing.Point(144, 64);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(224, 16);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Lesser General Public License Version 2.1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label3.Location = new System.Drawing.Point(8, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(320, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DirectShowLib Version : ";
+            // 
+            // AboutBoxWnd
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(372, 86);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBoxWnd";
+            this.ShowInTaskbar = false;
+            this.Text = "AboutBox";
+            this.ResumeLayout(false);
 
-    }
+        }
 		#endregion
 
     private void button1_Click(object sender, System.EventArgs e)
@@ -125,7 +125,7 @@ namespace DirectShowLib.Sample
 
     private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
     {
-      System.Diagnostics.Process.Start("http://www.apache.org/licenses/LICENSE-1.1");
+      System.Diagnostics.Process.Start("http://www.gnu.org/copyleft/lesser.txt");
     }
   }
 
