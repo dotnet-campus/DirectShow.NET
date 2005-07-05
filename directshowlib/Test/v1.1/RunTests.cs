157d241d7dd0c2509b2609c4b5e05911ab665f73
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-07-05 09:00:06 +0000

    V1.1 startup
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@304 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.1/RunTests.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace DirectShowLib.Test
+{
+	/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+    class Class1
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
