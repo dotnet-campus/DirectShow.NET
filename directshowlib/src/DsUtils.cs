6df948e9a61f0a33f5681dfa43505d9542eba5a6
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-07-10 11:00:03 +0000

    Add a DsRect class to use instead of System.Drawing.Rectangle
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@320 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/src/DsUtils.cs
+++ directshowlib/src/DsUtils.cs
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 using System;
 using System.Collections;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Reflection;
@@ -352,6 +353,132 @@ namespace DirectShowLib
         }
     }
 
+    /// <summary>
+    /// DirectShowLib.DsRect is a managed representation of the Win32 RECT structure.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public class DsRect
+    {
+      public int left;
+      public int top;
+      public int right;
+      public int bottom;
+
+      /// <summary>
+      /// Empty contructor. Initialize all fields to 0
+      /// </summary>
+      public DsRect()
+      {
+        this.left = 0;
+        this.top = 0;
+        this.right = 0;
+        this.bottom = 0;
+      }
+
+      /// <summary>
+      /// A parametred constructor. Initialize fields with given values.
+      /// </summary>
+      /// <param name="left">the left value</param>
+      /// <param name="top">the top value</param>
+      /// <param name="right">the right value</param>
+      /// <param name="bottom">the bottom value</param>
+      public DsRect(int left, int top, int right, int bottom)
+      {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+      }
+
+      /// <summary>
+      /// A parametred constructor. Initialize fields with a given <see cref="System.Drawing.Rectangle"/>.
+      /// </summary>
+      /// <param name="rectangle">A <see cref="System.Drawing.Rectangle"/></param>
+      /// <remarks>
+      /// Warning, DsRect define a rectangle by defining two of his corners and <see cref="System.Drawing.Rectangle"/> define a rectangle with his upper/left corner, his width and his height.
+      /// </remarks>
+      public DsRect(Rectangle rectangle)
+      {
+        this.left = rectangle.Left;
+        this.top = rectangle.Top;
+        this.right = rectangle.Right;
+        this.bottom = rectangle.Bottom;
+      }
+
+      /// <summary>
+      /// Provide de string representation of this DsRect instance
+      /// </summary>
+      /// <returns>A string formated like this : [left, top - right, bottom]</returns>
+      public override string ToString()
+      {
+        return string.Format("[{0}, {1} - {2}, {3}]", this.left, this.top, this.right, this.bottom);
+      }
+
+      public override int GetHashCode()
+      {
+        return this.left.GetHashCode() |
+              this.top.GetHashCode() |
+              this.right.GetHashCode() |
+              this.bottom.GetHashCode();
+      }
+
+      /// <summary>
+      /// Define implicit cast between DirectShowLib.DsRect and System.Drawing.Rectangle for languages supporting this feature.
+      /// VB.Net doesn't support implicit cast. <see cref="DirectShowLib.DsRect.ToRectangle"/> for similar functionality.
+      /// <code>
+      ///   // Define a new Rectangle instance
+      ///   Rectangle r = new Rectangle(0, 0, 100, 100);
+      ///   // Do implicit cast between Rectangle and DsRect
+      ///   DsRect dsR = r;
+      ///
+      ///   Console.WriteLine(dsR.ToString());
+      /// </code>
+      /// </summary>
+      /// <param name="r">a DsRect to be cast</param>
+      /// <returns>A casted System.Drawing.Rectangle</returns>
+      public static implicit operator Rectangle(DsRect r)
+      {
+        return r.ToRectangle();
+      }
+
+      /// <summary>
+      /// Define implicit cast between System.Drawing.Rectangle and DirectShowLib.DsRect for languages supporting this feature.
+      /// VB.Net doesn't support implicit cast. <see cref="DirectShowLib.DsRect.FromRectangle"/> for similar functionality.
+      /// <code>
+      ///   // Define a new DsRect instance
+      ///   DsRect dsR = new DsRect(0, 0, 100, 100);
+      ///   // Do implicit cast between DsRect and Rectangle
+      ///   Rectangle r = dsR;
+      ///
+      ///   Console.WriteLine(r.ToString());
+      /// </code>
+      /// </summary>
+      /// <param name="r">A System.Drawing.Rectangle to be cast</param>
+      /// <returns>A casted DsRect</returns>
+      public static implicit operator DsRect(Rectangle r)
+      {
+        return new DsRect(r);
+      }
+
+      /// <summary>
+      /// Get the System.Drawing.Rectangle equivalent to this DirectShowLib.DsRect instance.
+      /// </summary>
+      /// <returns>A System.Drawing.Rectangle</returns>
+      public Rectangle ToRectangle()
+      {
+        return new Rectangle(this.left, this.top, (this.right - this.left), (this.bottom - this.top));
+      }
+
+      /// <summary>
+      /// Get a new DirectShowLib.DsRect instance for a given <see cref="System.Drawing.Rectangle"/>
+      /// </summary>
+      /// <param name="r">The <see cref="System.Drawing.Rectangle"/> used to initialize this new DirectShowLib.DsGuid</param>
+      /// <returns>A new instance of DirectShowLib.DsGuid</returns>
+      public static DsRect FromRectangle(Rectangle r)
+      {
+        return new DsRect(r);
+      }
+    }
     #endregion
 
     #region Utility Classes
