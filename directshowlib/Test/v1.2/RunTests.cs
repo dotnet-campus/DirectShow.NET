a1731c3c2d2694dd57768b9d254c0648835b7033
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-08-30 02:19:36 +0000

    Startup for v1.2
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@511 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.2/RunTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Forms;
+
+namespace DirectShowLib.Test
+{
+    class Class1
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // IAMTimelineTest t1 = new IAMTimelineTest();            // t1.DoTests();
+            // IAMTimelineCompTest t2 = new IAMTimelineCompTest();            // t2.DoTests();
+            // IAMTimelineEffectTest t3 = new IAMTimelineEffectTest();            // t3.DoTests();
+            // IAMTimelineEffectableTest t4 = new IAMTimelineEffectableTest();            // t4.DoTests();
+            // IAMTimelineGroupTest t5 = new IAMTimelineGroupTest();            // t5.DoTests();
+            // IAMTimelineObjTest t6 = new IAMTimelineObjTest();            // t6.DoTests();
+            // IAMTimelineSrcTest t7 = new IAMTimelineSrcTest();            // t7.DoTests();
+            // IAMTimelineTrackTest t8 = new IAMTimelineTrackTest();            // t8.DoTests();
+            // IMediaDetTest t9 = new IMediaDetTest();            // t9.DoTests();
+            // IPropertySetterTest t10 = new IPropertySetterTest();            // t10.DoTests();
+            // IRenderEngineTest t11 = new IRenderEngineTest();            // t11.DoTests();
+            // IXml2DexTest t12 = new IXml2DexTest();            // t12.DoTests();
+        }
+    }
+}
