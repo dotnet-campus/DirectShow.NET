24c5aafb92d5c22993baf5797e84e4bb70f7cabd
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-09-10 10:52:08 +0000

    IDVBSLocator test framework
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@546 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.2/IDVBSLocatorTest.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using DirectShowLib.BDA;
+
+namespace DirectShowLib.Test
+{
+	public class IDVBSLocatorTest
+	{
+    private IDVBSLocator locator;
+    
+    public IDVBSLocatorTest()
+		{
+		}
+
+    public void DoTests()
+    {
+      try
+      {
+        locator = (IDVBSLocator) new DVBSLocator();
+
+        TestAzimuth();
+        TestElevation();
+        TestOrbitalPosition();
+        TestSignalPolarisation();
+        TestWestPosition();
+      }
+      finally
+      {
+        Marshal.ReleaseComObject(locator);
+      }
+    }
+
+    private void TestAzimuth()
+    {
+      int hr = 0;
+      int az;
+
+      hr = locator.put_Azimuth(900);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_Azimuth(out az);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(az == 900, "IDVBSLocator.get_Azimuth / put_Azimuth");
+    }
+
+    private void TestElevation()
+    {
+      int hr = 0;
+      int elev;
+
+      hr = locator.put_Elevation(450);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_Elevation(out elev);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(elev == 450, "IDVBSLocator.get_Elevation / put_Elevation");
+    }
+
+    private void TestOrbitalPosition()
+    {
+      int hr = 0;
+      int longitude;
+
+      hr = locator.put_OrbitalPosition(1800);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_OrbitalPosition(out longitude);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(longitude == 1800, "IDVBSLocator.get_OrbitalPosition / put_OrbitalPosition");
+    }
+
+    private void TestSignalPolarisation()
+    {
+      int hr = 0;
+      Polarisation pol;
+
+      hr = locator.put_SignalPolarisation(Polarisation.LINEAR_H);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_SignalPolarisation(out pol);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(pol == Polarisation.LINEAR_H, "IDVBSLocator.get_SignalPolarisation / put_SignalPolarisation");
+    }
+
+    private void TestWestPosition()
+    {
+      int hr = 0;
+      bool westPos;
+
+      hr = locator.put_WestPosition(true);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_WestPosition(out westPos);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(westPos == true, "IDVBSLocator.get_WestPosition / put_WestPosition");
+
+      hr = locator.put_WestPosition(false);
+      DsError.ThrowExceptionForHR(hr);
+
+      hr = locator.get_WestPosition(out westPos);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(westPos == false, "IDVBSLocator.get_WestPosition / put_WestPosition");
+    }
+  
+  }
+}
