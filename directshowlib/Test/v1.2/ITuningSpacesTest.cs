4c01d7a7f905ec04065748a29d22e52be30746d4
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-09-12 14:39:32 +0000

    ITuningSpaces test framework
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@558 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.2/ITuningSpacesTest.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using DirectShowLib.BDA;
+
+namespace DirectShowLib.Test
+{
+	public class ITuningSpacesTest
+	{
+    ITuningSpaceContainer tsContainer;
+    ITuningSpaces tuningSpaces;
+
+		public ITuningSpacesTest()
+		{
+		}
+
+    public void DoTests()
+    {
+      int hr = 0;
+
+      try
+      {
+        Guid searchClsid = typeof(ATSCTuningSpace).GUID;
+        tsContainer = (ITuningSpaceContainer) new SystemTuningSpaces();
+
+        hr = tsContainer._TuningSpacesForCLSID(searchClsid, out tuningSpaces);
+        DsError.ThrowExceptionForHR(hr);
+
+        Testget__NewEnum();
+        Testget_Count();
+        Testget_EnumTuningSpaces();
+        Testget_Item();
+      }
+      finally
+      {
+        Marshal.ReleaseComObject(tsContainer);
+        Marshal.ReleaseComObject(tuningSpaces);
+      }
+    }
+
+    private void Testget__NewEnum()
+    {
+      int hr = 0;
+      UCOMIEnumVARIANT enumv;
+
+      hr = tuningSpaces.get__NewEnum(out enumv);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert((hr == 0) && (enumv != null), "ITuningSpaces.get__NewEnum");
+
+      Marshal.ReleaseComObject(enumv);
+    }
+
+    private void Testget_Count()
+    {
+      int hr = 0;
+      int count;
+
+      hr = tuningSpaces.get_Count(out count);
+      DsError.ThrowExceptionForHR(hr);
+
+      // There is normaly at least 1 ATSC system tuning space...
+      Debug.Assert((hr == 0) && (count > 0), "ITuningSpaces.get_Count");
+    }
+
+    private void Testget_EnumTuningSpaces()
+    {
+      int hr = 0;
+      IEnumTuningSpaces enumTS;
+
+      hr = tuningSpaces.get_EnumTuningSpaces(out enumTS);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert((hr == 0) && (enumTS != null), "ITuningSpaces.get_EnumTuningSpaces");
+
+      Marshal.ReleaseComObject(enumTS);
+    }
+
+    private void Testget_Item()
+    {
+      int hr = 0;
+      ITuningSpace ts;
+
+      // 3rd system tuning space is the ATSC one...
+      hr = tuningSpaces.get_Item(3, out ts);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert((hr == 0) && (ts != null), "ITuningSpaces.get_EnumTuningSpaces");
+
+      Marshal.ReleaseComObject(ts);
+    }
+
+	}
+}
