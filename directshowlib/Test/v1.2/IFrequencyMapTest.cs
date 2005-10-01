0fcafb5fccfeb34cdc322bad5d1d6958576d09b3
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-10-01 17:21:45 +0000

    IFrequencyMap test framework (not finished)
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@599 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Test/v1.2/IFrequencyMapTest.cs
+++ directshowlib/Test/v1.2/IFrequencyMapTest.cs
@@ -10,6 +10,21 @@ namespace DirectShowLib.Test
 {
 	public class IFrequencyMapTest
 	{
+    BDANetworkType networkType = BDANetworkType.DVBT;
+
+    IFilterGraph2 graphBuilder;
+    DsROTEntry rot;
+
+    IBaseFilter networkProvider;
+    ITuner tuner;
+
+    ITuningSpace tuningSpace;
+    ITuneRequest tuneRequest;
+
+    IBaseFilter bdaTuner, bdaCapture;
+    IBaseFilter mpeg2Demux;
+    IBaseFilter bdaTIF, bdaSecTab;
+
     IFrequencyMap freqMap;
 
 		public IFrequencyMapTest()
@@ -20,19 +35,98 @@ namespace DirectShowLib.Test
     {
       try
       {
-        freqMap = (IFrequencyMap) new DVBTNetworkProvider();
+        BuildGraph();
 
         TestCountryCode();
         Testget_CountryCodeList();
-        Testget_DefaultFrequencyMapping();
-        TestFrequencyMapping();
+//        Testget_DefaultFrequencyMapping();
+//        TestFrequencyMapping();
 
 
       }
       finally
       {
-        Marshal.ReleaseComObject(freqMap);
+        Marshal.ReleaseComObject(bdaSecTab);
+        Marshal.ReleaseComObject(bdaTIF);
+        Marshal.ReleaseComObject(mpeg2Demux);
+        Marshal.ReleaseComObject(bdaCapture);
+        Marshal.ReleaseComObject(bdaTuner);
+        Marshal.ReleaseComObject(tuneRequest);
+        Marshal.ReleaseComObject(tuningSpace);
+        Marshal.ReleaseComObject(networkProvider);
+        Marshal.ReleaseComObject(graphBuilder);
       }
+    }
+
+    private void BuildGraph()
+    {
+      int hr = 0;
+
+      graphBuilder = (IFilterGraph2) new FilterGraph();
+      rot = new DsROTEntry(graphBuilder);
+
+      ICaptureGraphBuilder2 capBuilder = (ICaptureGraphBuilder2) new CaptureGraphBuilder2();
+      capBuilder.SetFiltergraph(graphBuilder);
+
+      // Get the BDA network provider specific for this given network type
+      networkProvider = BDAUtils.GetNetworkProvider(networkType);
+
+      hr = graphBuilder.AddFilter(networkProvider, "BDA Network Provider");
+      DsError.ThrowExceptionForHR(hr);
+
+      tuner = (ITuner) networkProvider;
+
+      // Get a tuning space for this network type
+      tuningSpace = BDAUtils.GetTuningSpace(networkType);
+
+      hr = tuner.put_TuningSpace(tuningSpace);
+      DsError.ThrowExceptionForHR(hr);
+
+      // Create a tune request from this tuning space
+      tuneRequest = BDAUtils.CreateTuneRequest(tuningSpace);
+
+      // Is it okay ?
+      hr = tuner.Validate(tuneRequest);
+      if (hr == 0)
+      {
+        // Set it
+        hr = tuner.put_TuneRequest(tuneRequest);
+        DsError.ThrowExceptionForHR(hr);
+
+        // found a BDA Tuner and a BDA Capture that can connect to this network provider
+        BDAUtils.AddBDATunerAndDemodulatorToGraph(graphBuilder, networkProvider, out bdaTuner, out bdaCapture);
+
+        if ((bdaTuner != null) && (bdaCapture != null)) 
+        {
+          // Create and add the mpeg2 demux
+          mpeg2Demux = (IBaseFilter) new MPEG2Demultiplexer();
+
+          hr = graphBuilder.AddFilter(mpeg2Demux, "MPEG2 Demultiplexer");
+          DsError.ThrowExceptionForHR(hr);
+
+          // connect it to the BDA Capture
+          hr = capBuilder.RenderStream(null, null, bdaCapture, null, mpeg2Demux);
+          DsError.ThrowExceptionForHR(hr);
+
+          // Add the two mpeg2 transport stream helper filters
+          BDAUtils.AddTransportStreamFiltersToGraph(graphBuilder, out bdaTIF, out bdaSecTab);
+
+          if ((bdaTIF != null) && (bdaSecTab != null))
+          {
+            // Render all the output pins of the demux (missing filters are added)
+            for (int i = 0; i < 5; i++)
+            {
+              IPin pin = DsFindPin.ByDirection(mpeg2Demux, PinDirection.Output, i);
+
+              hr = graphBuilder.Render(pin);
+              Marshal.ReleaseComObject(pin);
+            }
+          }
+        }
+      }
+
+      // Currently only DVBT Network Provider support this interface...
+      freqMap = (IFrequencyMap) networkProvider;
     }
 
     private void TestCountryCode()
@@ -40,31 +134,31 @@ namespace DirectShowLib.Test
       int hr = 0;
       int countryCode;
 
-      // This method is not documented to not been implemented but this the case on my machine
-      hr = freqMap.get_CountryCode(out countryCode);
-      
-      Debug.Assert(hr == -2147467263, "IFrequencyMap.get_CountryCode");
-
       // Vive la France !!!
       hr = freqMap.put_CountryCode(33);
+      DsError.ThrowExceptionForHR(hr);
 
-      // This method is not documented to not been implemented but this the case on my machine
-      Debug.Assert(hr == -2147467263, "IFrequencyMap.get_CountryCode");
+      hr = freqMap.get_CountryCode(out countryCode);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(countryCode == 33, "IFrequencyMap.get_CountryCode / put_CountryCode");
     }
 
     private void Testget_CountryCodeList()
     {
       int hr = 0;
       int entryCount;
+      IntPtr entriesPtr;
       int[] entries;
 
-      hr = freqMap.get_CountryCodeList(out entryCount, null);
+      hr = freqMap.get_CountryCodeList(out entryCount, out entriesPtr);
+      DsError.ThrowExceptionForHR(hr);
 
       entries = new int[entryCount];
-      hr = freqMap.get_CountryCodeList(out entryCount, entries);
+      Marshal.Copy(entriesPtr, entries, 0, entryCount);
+      Marshal.FreeCoTaskMem(entriesPtr);
 
-      // This method is not documented to not been implemented but this the case on my machine
-      Debug.Assert(hr == -2147467263, "IFrequencyMap.get_CountryCodeList");
+      Debug.Assert(entryCount > 0, "IFrequencyMap.get_CountryCodeList");
     }
 
     private void Testget_DefaultFrequencyMapping()
