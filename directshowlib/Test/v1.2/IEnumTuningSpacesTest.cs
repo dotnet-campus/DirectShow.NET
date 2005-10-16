450242562078d60c64f4d0ad704a538a4831029b
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-10-16 13:55:04 +0000

    IEnumTuningSpaces test framework
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@635 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.2/IEnumTuningSpacesTest.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using DirectShowLib.BDA;
+
+namespace DirectShowLib.Test
+{
+	public class IEnumTuningSpacesTest
+	{
+    IEnumTuningSpaces enumTS;
+
+		public IEnumTuningSpacesTest()
+		{
+		}
+
+    public void DoTests()
+    {
+      try
+      {
+        Config();
+
+        TestClone();
+        TestNext();
+        TestReset();
+        TestSkip();
+      }
+      finally
+      {
+        Marshal.ReleaseComObject(enumTS);
+      }
+    }
+
+    private void Config()
+    {
+      int hr = 0;
+      ITuningSpaceContainer tsCont = (ITuningSpaceContainer) new SystemTuningSpaces();
+
+      hr = tsCont.get_EnumTuningSpaces(out enumTS);
+      DsError.ThrowExceptionForHR(hr);
+
+      Marshal.ReleaseComObject(tsCont);
+    }
+
+    private void TestClone()
+    {
+      int hr = 0;
+      IEnumTuningSpaces clone;
+
+      hr = enumTS.Clone(out clone);
+      DsError.ThrowExceptionForHR(hr);
+
+      Debug.Assert(clone != null, "IEnumTuningSpaces.Clone");
+
+      Marshal.ReleaseComObject(clone);
+    }
+
+    private void TestNext()
+    {
+      int hr = 0;
+      ITuningSpace[] ts = new ITuningSpace[5];
+      int fetched;
+      int validObj = 0;
+
+      hr = enumTS.Next(ts.Length, ts, out fetched);
+      DsError.ThrowExceptionForHR(hr);
+
+      for(int i = 0; i < fetched; i++)
+      {
+        if (ts[i] != null)
+        {
+          validObj++;
+          Marshal.ReleaseComObject(ts[i]);
+        }
+      }
+
+      Debug.Assert(validObj == fetched, "IEnumTuningSpaces.Next");
+    }
+
+    private void TestReset()
+    {
+      int hr = 0;
+
+      hr = enumTS.Reset();
+
+      Debug.Assert(hr == 0, "IEnumTuningSpaces.Reset");
+    }
+
+    private void TestSkip()
+    {
+      int hr = 0;
+
+      hr = enumTS.Skip(4);
+
+      Debug.Assert(hr == 0, "IEnumTuningSpaces.Skip");
+    }
+
+  }
+}
