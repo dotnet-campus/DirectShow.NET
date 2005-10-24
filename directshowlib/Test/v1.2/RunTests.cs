e1d4a208361923ca1d9ed0fb7dc2a53e00fb5a66
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-10-24 02:58:49 +0000

    Added IComponents*
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@652 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Test/v1.2/RunTests.cs
+++ directshowlib/Test/v1.2/RunTests.cs
@@ -8,18 +8,45 @@ namespace DirectShowLib.Test
         [STAThread]
         static void Main(string[] args)
         {
-            // IAMTimelineTest t1 = new IAMTimelineTest();            // t1.DoTests();
-            // IAMTimelineCompTest t2 = new IAMTimelineCompTest();            // t2.DoTests();
-            // IAMTimelineEffectTest t3 = new IAMTimelineEffectTest();            // t3.DoTests();
-            // IAMTimelineEffectableTest t4 = new IAMTimelineEffectableTest();            // t4.DoTests();
-            // IAMTimelineGroupTest t5 = new IAMTimelineGroupTest();            // t5.DoTests();
-            // IAMTimelineObjTest t6 = new IAMTimelineObjTest();            // t6.DoTests();
-            // IAMTimelineSrcTest t7 = new IAMTimelineSrcTest();            // t7.DoTests();
-            // IAMTimelineTrackTest t8 = new IAMTimelineTrackTest();            // t8.DoTests();
-            // IMediaDetTest t9 = new IMediaDetTest();            // t9.DoTests();
-            // IPropertySetterTest t10 = new IPropertySetterTest();            // t10.DoTests();
-            // IRenderEngineTest t11 = new IRenderEngineTest();            // t11.DoTests();
-            // IXml2DexTest t12 = new IXml2DexTest();            // t12.DoTests();            // IMediaLocatorTest t13 = new IMediaLocatorTest();            // t13.DoTests();
+            // IAMTimelineTest t1 = new IAMTimelineTest();
+            // t1.DoTests();
+
+            // IAMTimelineCompTest t2 = new IAMTimelineCompTest();
+            // t2.DoTests();
+
+            // IAMTimelineEffectTest t3 = new IAMTimelineEffectTest();
+            // t3.DoTests();
+
+            // IAMTimelineEffectableTest t4 = new IAMTimelineEffectableTest();
+            // t4.DoTests();
+
+            // IAMTimelineGroupTest t5 = new IAMTimelineGroupTest();
+            // t5.DoTests();
+
+            // IAMTimelineObjTest t6 = new IAMTimelineObjTest();
+            // t6.DoTests();
+
+            // IAMTimelineSrcTest t7 = new IAMTimelineSrcTest();
+            // t7.DoTests();
+
+            // IAMTimelineTrackTest t8 = new IAMTimelineTrackTest();
+            // t8.DoTests();
+
+            // IMediaDetTest t9 = new IMediaDetTest();
+            // t9.DoTests();
+
+            // IPropertySetterTest t10 = new IPropertySetterTest();
+            // t10.DoTests();
+
+            // IRenderEngineTest t11 = new IRenderEngineTest();
+            // t11.DoTests();
+
+            // IXml2DexTest t12 = new IXml2DexTest();
+            // t12.DoTests();
+
+            // IMediaLocatorTest t13 = new IMediaLocatorTest();
+            // t13.DoTests();
+
             // MoTest t14 = new MoTest();
             // t14.DoTests();
 
@@ -58,28 +85,80 @@ namespace DirectShowLib.Test
 
             // IEnumDMOTest t26 = new IEnumDMOTest();
             // t26.DoTests();
-            // IBDA_AutoDemodulateTest t30 = new IBDA_AutoDemodulateTest();            // t30.DoTests();
-            // IBDA_LNBInfoTest t31 = new IBDA_LNBInfoTest();            // t31.DoTests();
-            // IBDA_PinControlTest t32 = new IBDA_PinControlTest();            // t32.DoTests();
-            // IBDA_SignalStatisticsTest t33 = new IBDA_SignalStatisticsTest();            // t33.DoTests();
-            // IBDA_TopologyTest t34 = new IBDA_TopologyTest();            // t34.DoTests();
-            // ICreatePropBagOnRegKeyTest t35 = new ICreatePropBagOnRegKeyTest();            // t35.DoTests();
-            // IDVBSLocatorTest t36 = new IDVBSLocatorTest();            // t36.DoTests();
-            // IDVBSTuningSpaceTest t37 = new IDVBSTuningSpaceTest();            // t37.DoTests();
-            // IDVBTLocatorTest t38 = new IDVBTLocatorTest();            // t38.DoTests();
-            // IDVBTuneRequestTest t39 = new IDVBTuneRequestTest();            // t39.DoTests();
-            // IDVBTuningSpaceTest t40 = new IDVBTuningSpaceTest();            // t40.DoTests();
-            // IDVBTuningSpace2Test t41 = new IDVBTuningSpace2Test();            // t41.DoTests();
-            // IEnumTuneRequestsTest t42 = new IEnumTuneRequestsTest();            // t42.DoTests();
-            // IEnumTuningSpacesTest t43 = new IEnumTuningSpacesTest();            // t43.DoTests();
-            // IFrequencyMapTest t44 = new IFrequencyMapTest();            // t44.DoTests();
-            // ILocatorTest t45 = new ILocatorTest();            // t45.DoTests();
-            // IMPEG2TuneRequestTest t46 = new IMPEG2TuneRequestTest();            // t46.DoTests();
-            // ITunerTest t47 = new ITunerTest();            // t47.DoTests();
-            // ITuneRequestTest t48 = new ITuneRequestTest();            // t48.DoTests();
-            // ITuningSpaceTest t49 = new ITuningSpaceTest();            // t49.DoTests();
-            // ITuningSpaceContainerTest t50 = new ITuningSpaceContainerTest();            // t50.DoTests();
-            // ITuningSpacesTest t51 = new ITuningSpacesTest();            // t51.DoTests();
+
+            // IBDA_AutoDemodulateTest t30 = new IBDA_AutoDemodulateTest();
+            // t30.DoTests();
+
+            // IBDA_LNBInfoTest t31 = new IBDA_LNBInfoTest();
+            // t31.DoTests();
+
+            // IBDA_PinControlTest t32 = new IBDA_PinControlTest();
+            // t32.DoTests();
+
+            // IBDA_SignalStatisticsTest t33 = new IBDA_SignalStatisticsTest();
+            // t33.DoTests();
+
+            // IBDA_TopologyTest t34 = new IBDA_TopologyTest();
+            // t34.DoTests();
+            // ICreatePropBagOnRegKeyTest t35 = new ICreatePropBagOnRegKeyTest();
+            // t35.DoTests();
+
+            // IDVBSLocatorTest t36 = new IDVBSLocatorTest();
+            // t36.DoTests();
+
+            // IDVBSTuningSpaceTest t37 = new IDVBSTuningSpaceTest();
+            // t37.DoTests();
+
+            // IDVBTLocatorTest t38 = new IDVBTLocatorTest();
+            // t38.DoTests();
+
+            // IDVBTuneRequestTest t39 = new IDVBTuneRequestTest();
+            // t39.DoTests();
+
+            // IDVBTuningSpaceTest t40 = new IDVBTuningSpaceTest();
+            // t40.DoTests();
+
+            // IDVBTuningSpace2Test t41 = new IDVBTuningSpace2Test();
+            // t41.DoTests();
+
+            // IEnumTuneRequestsTest t42 = new IEnumTuneRequestsTest();
+            // t42.DoTests();
+
+            // IEnumTuningSpacesTest t43 = new IEnumTuningSpacesTest();
+            // t43.DoTests();
+
+            // IFrequencyMapTest t44 = new IFrequencyMapTest();
+            // t44.DoTests();
+
+            // ILocatorTest t45 = new ILocatorTest();
+            // t45.DoTests();
+
+            // IMPEG2TuneRequestTest t46 = new IMPEG2TuneRequestTest();
+            // t46.DoTests();
+
+            // ITunerTest t47 = new ITunerTest();
+            // t47.DoTests();
+
+            // ITuneRequestTest t48 = new ITuneRequestTest();
+            // t48.DoTests();
+
+            // ITuningSpaceTest t49 = new ITuningSpaceTest();
+            // t49.DoTests();
+
+            // ITuningSpaceContainerTest t50 = new ITuningSpaceContainerTest();
+            // t50.DoTests();
+
+            // ITuningSpacesTest t51 = new ITuningSpacesTest();
+            // t51.DoTests();
+
+            // IComponentTypesTest t52 = new IComponentTypesTest();
+            // t52.DoTests();
+
+            // IEnumComponentTypesTest t53 = new IEnumComponentTypesTest();
+            // t53.DoTests();
+
+            // IComponentTypeTest t54 = new IComponentTypeTest();
+            // t54.DoTests();
         }
     }
 }
