50ca3f2d8638f2cd09b613039f98d7cb2dbb2e2b
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-10-29 12:15:39 +0000

    Change the capitalisation of BDA enums
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@668 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/src/BDATypes.cs
+++ directshowlib/src/BDATypes.cs
@@ -37,26 +37,26 @@ namespace DirectShowLib.BDA
 	/// </summary>
 	public enum BDAEventID
 	{
-		SIGNAL_LOSS = 0,
-		SIGNAL_LOCK,
-		DATA_START,
-		DATA_STOP,
-		CHANNEL_ACQUIRED,
-		CHANNEL_LOST,
-		CHANNEL_SOURCE_CHANGED,
-		CHANNEL_ACTIVATED,
-		CHANNEL_DEACTIVATED,
-		SUBCHANNEL_ACQUIRED,
-		SUBCHANNEL_LOST,
-		SUBCHANNEL_SOURCE_CHANGED,
-		SUBCHANNEL_ACTIVATED,
-		SUBCHANNEL_DEACTIVATED,
-		ACCESS_GRANTED,
-		ACCESS_DENIED,
-		OFFER_EXTENDED,
-		PURCHASE_COMPLETED,
-		SMART_CARD_INSERTED,
-		SMART_CARD_REMOVED
+		SignalLoss = 0,
+		SignalLock,
+		DataStart,
+		DataStop,
+		ChannelAcquired,
+		ChannelLost,
+		ChannelSourceChanged,
+		ChannelActivated,
+		ChannelDeactivated,
+		SubChannelAcquired,
+		SubChannelLost,
+		SubChannelSourceChanged,
+		SubChannelActivated,
+		SubChannelDeactivated,
+		AccessGranted,
+		AccessDenied,
+		OfferExtended,
+		PurchaseCompleted,
+		SmartCardInserted,
+		SmartCardRemoved
 	}
 
 	/// <summary>
@@ -64,24 +64,24 @@ namespace DirectShowLib.BDA
 	/// </summary>
 	public enum MPEG2StreamType
 	{
-		BDA_UNITIALIZED_MPEG2STREAMTYPE = -1,
+		BdaUninitializedMpeg2StreamType = -1,
 		Reserved1 = 0x0,
-		ISO_IEC_11172_2_VIDEO = Reserved1 + 1,
-		ISO_IEC_13818_2_VIDEO = ISO_IEC_11172_2_VIDEO + 1,
-		ISO_IEC_11172_3_AUDIO = ISO_IEC_13818_2_VIDEO + 1,
-		ISO_IEC_13818_3_AUDIO = ISO_IEC_11172_3_AUDIO + 1,
-		ISO_IEC_13818_1_PRIVATE_SECTION = ISO_IEC_13818_3_AUDIO + 1,
-		ISO_IEC_13818_1_PES = ISO_IEC_13818_1_PRIVATE_SECTION + 1,
-		ISO_IEC_13522_MHEG = ISO_IEC_13818_1_PES + 1,
-		ANNEX_A_DSM_CC = ISO_IEC_13522_MHEG + 1,
-		ITU_T_REC_H_222_1 = ANNEX_A_DSM_CC + 1,
-		ISO_IEC_13818_6_TYPE_A = ITU_T_REC_H_222_1 + 1,
-		ISO_IEC_13818_6_TYPE_B = ISO_IEC_13818_6_TYPE_A + 1,
-		ISO_IEC_13818_6_TYPE_C = ISO_IEC_13818_6_TYPE_B + 1,
-		ISO_IEC_13818_6_TYPE_D = ISO_IEC_13818_6_TYPE_C + 1,
-		ISO_IEC_13818_1_AUXILIARY = ISO_IEC_13818_6_TYPE_D + 1,
-		ISO_IEC_13818_1_RESERVED = ISO_IEC_13818_1_AUXILIARY + 1,
-		USER_PRIVATE = ISO_IEC_13818_1_RESERVED + 1
+		IsoIec11172_2_Video = Reserved1 + 1,
+		IsoIec13818_2_Video = IsoIec11172_2_Video + 1,
+		IsoIec11172_3_Audio = IsoIec13818_2_Video + 1,
+		IsoIec13818_3_Audio = IsoIec11172_3_Audio + 1,
+		IsoIec13818_1_PrivateSection = IsoIec13818_3_Audio + 1,
+		IsoIec13818_1_Pes = IsoIec13818_1_PrivateSection + 1,
+		IsoIec13522_Mheg = IsoIec13818_1_Pes + 1,
+		AnnexADsmCC = IsoIec13522_Mheg + 1,
+		ItuTRecH222_1 = AnnexADsmCC + 1,
+		IsoIec13818_6_TypeA = ItuTRecH222_1 + 1,
+		IsoIec13818_6_TypeB = IsoIec13818_6_TypeA + 1,
+		IsoIec13818_6_TypeC = IsoIec13818_6_TypeB + 1,
+		IsoIec13818_6_TypeD = IsoIec13818_6_TypeC + 1,
+		IsoIec13818_1_Auxiliary = IsoIec13818_6_TypeD + 1,
+		IsoIec13818_1_Reserved = IsoIec13818_1_Auxiliary + 1,
+		UserPrivate = IsoIec13818_1_Reserved + 1
 	}
 
 	/// <summary>
@@ -135,11 +135,11 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum FECMethod
     {
-        METHOD_NOT_SET = -1,
-        METHOD_NOT_DEFINED = 0,
-        VITERBI = 1, // FEC is a Viterbi Binary Convolution.
-        RS_204_188, // The FEC is Reed-Solomon 204/188 (outer FEC)
-        MAX,
+        MethodNotSet = -1,
+        MethodNotDefined = 0,
+        Viterbi = 1, // FEC is a Viterbi Binary Convolution.
+        RS204_188, // The FEC is Reed-Solomon 204/188 (outer FEC)
+        Max,
     }
 
     /// <summary>
@@ -147,17 +147,17 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum BinaryConvolutionCodeRate
     {
-        RATE_NOT_SET = -1,
-        RATE_NOT_DEFINED = 0,
-        RATE_1_2 = 1, // 1/2
-        RATE_2_3, // 2/3
-        RATE_3_4, // 3/4
-        RATE_3_5,
-        RATE_4_5,
-        RATE_5_6, // 5/6
-        RATE_5_11,
-        RATE_7_8, // 7/8
-        RATE_MAX
+        RateNotSet = -1,
+        RateNotDefined = 0,
+        Rate1_2 = 1, // 1/2
+        Rate2_3, // 2/3
+        Rate3_4, // 3/4
+        Rate3_5,
+        Rate4_5,
+        Rate5_6, // 5/6
+        Rate5_11,
+        Rate7_8, // 7/8
+        RateMax
     }
 
     /// <summary>
@@ -165,13 +165,13 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum Polarisation
     {
-        NOT_SET = -1,
-        NOT_DEFINED = 0,
-        LINEAR_H = 1, // Linear horizontal polarisation
-        LINEAR_V, // Linear vertical polarisation
-        CIRCULAR_L, // Circular left polarisation
-        CIRCULAR_R, // Circular right polarisation
-        MAX,
+        NotSet = -1,
+        NotDefined = 0,
+        LinearH = 1, // Linear horizontal polarisation
+        LinearV, // Linear vertical polarisation
+        CircularL, // Circular left polarisation
+        CircularR, // Circular right polarisation
+        Max,
     }
 
     /// <summary>
@@ -179,12 +179,12 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum SpectralInversion
     {
-        NOT_SET = -1,
-        NOT_DEFINED = 0,
-        AUTOMATIC = 1,
-        NORMAL,
-        INVERTED,
-        MAX
+        NotSet = -1,
+        NotDefined = 0,
+        Automatic = 1,
+        Normal,
+        Inverted,
+        Max
     }
 
     /// <summary>
@@ -192,35 +192,35 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum ModulationType
     {
-        MOD_NOT_SET = -1,
-        MOD_NOT_DEFINED = 0,
-        MOD_16QAM = 1,
-        MOD_32QAM,
-        MOD_64QAM,
-        MOD_80QAM,
-        MOD_96QAM,
-        MOD_112QAM,
-        MOD_128QAM,
-        MOD_160QAM,
-        MOD_192QAM,
-        MOD_224QAM,
-        MOD_256QAM,
-        MOD_320QAM,
-        MOD_384QAM,
-        MOD_448QAM,
-        MOD_512QAM,
-        MOD_640QAM,
-        MOD_768QAM,
-        MOD_896QAM,
-        MOD_1024QAM,
-        MOD_QPSK,
-        MOD_BPSK,
-        MOD_OQPSK,
-        MOD_8VSB,
-        MOD_16VSB,
-        MOD_ANALOG_AMPLITUDE, // std am
-        MOD_ANALOG_FREQUENCY, // std fm
-        MOD_MAX
+        ModNotSet = -1,
+        ModNotDefined = 0,
+        Mod16Qam = 1,
+        Mod32Qam,
+        Mod64Qam,
+        Mod80Qam,
+        Mod96Qam,
+        Mod112Qam,
+        Mod128Qam,
+        Mod160Qam,
+        Mod192Qam,
+        Mod224Qam,
+        Mod256Qam,
+        Mod320Qam,
+        Mod384Qam,
+        Mod448Qam,
+        Mod512Qam,
+        Mod640Qam,
+        Mod768Qam,
+        Mod896Qam,
+        Mod1024Qam,
+        ModQpsk,
+        ModBpsk,
+        ModOqpsk,
+        Mod8Vsb,
+        Mod16Vsb,
+        ModAnalogAmplitude, // std am
+        ModAnalogFrequency, // std fm
+        ModMax
     }
 
     /// <summary>
@@ -238,12 +238,12 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum HierarchyAlpha
     {
-        HALPHA_NOT_SET = -1,
-        HALPHA_NOT_DEFINED = 0,
-        HALPHA_1 = 1, // Hierarchy alpha is 1.
-        HALPHA_2, // Hierarchy alpha is 2.
-        HALPHA_4, // Hierarchy alpha is 4.
-        HALPHA_MAX,
+        HAlphaNotSet = -1,
+        HAlphaNotDefined = 0,
+        HAlpha1 = 1, // Hierarchy alpha is 1.
+        HAlpha2, // Hierarchy alpha is 2.
+        HAlpha4, // Hierarchy alpha is 4.
+        HAlphaMax,
     }
 
     /// <summary>
@@ -251,13 +251,13 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum GuardInterval
     {
-        GUARD_NOT_SET = -1,
-        GUARD_NOT_DEFINED = 0,
-        GUARD_1_32 = 1, // Guard interval is 1/32
-        GUARD_1_16, // Guard interval is 1/16
-        GUARD_1_8, // Guard interval is 1/8
-        GUARD_1_4, // Guard interval is 1/4
-        GUARD_MAX,
+        GuardNotSet = -1,
+        GuardNotDefined = 0,
+        Guard1_32 = 1, // Guard interval is 1/32
+        Guard1_16, // Guard interval is 1/16
+        Guard1_8, // Guard interval is 1/8
+        Guard1_4, // Guard interval is 1/4
+        GuardMax,
     }
 
     /// <summary>
@@ -265,11 +265,11 @@ namespace DirectShowLib.BDA
     /// </summary>
     public enum TransmissionMode
     {
-        MODE_NOT_SET = -1,
-        MODE_NOT_DEFINED = 0,
-        MODE_2K = 1, // Transmission uses 1705 carriers (use a 2K FFT)
-        MODE_8K, // Transmission uses 6817 carriers (use an 8K FFT)
-        MODE_MAX,
+        ModeNotSet = -1,
+        ModeNotDefined = 0,
+        Mode2K = 1, // Transmission uses 1705 carriers (use a 2K FFT)
+        Mode8K, // Transmission uses 6817 carriers (use an 8K FFT)
+        ModeMax,
     }
 
     /// <summary>
