e2ab5dc120763b6765c5bf81887cbb14662ffcc8
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-11-07 02:52:50 +0000

    Framework for v1.3
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@699 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.3/RunTests.cs
@@ -0,0 +1,302 @@
+using System;
+
+namespace DirectShowLib.Test
+{
+    class Class1
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // IAMGraphBuilderCallbackTest t0 = new IAMGraphBuilderCallbackTest();
+            // t0.DoTests();
+
+            // IAMCameraControlTest t1 = new IAMCameraControlTest();
+            // t1.DoTests();
+
+            // IAMDirectSoundTest t2 = new IAMDirectSoundTest();
+            // t2.DoTests();
+
+            // IAMLine21DecoderTest t3 = new IAMLine21DecoderTest();
+            // t3.DoTests();
+
+            // IFilterMapper2Test t4 = new IFilterMapper2Test();
+            // t4.DoTests();
+
+            // IGraphConfigTest t5 = new IGraphConfigTest();
+            // t5.DoTests();
+
+            // IGraphConfigCallbackTest t6 = new IGraphConfigCallbackTest();
+            // t6.DoTests();
+
+            // IPinConnectionTest t7 = new IPinConnectionTest();
+            // t7.DoTests();
+
+            // IPinFlowControlTest t8 = new IPinFlowControlTest();
+            // t8.DoTests();
+
+            // IAMChannelInfoTest t9 = new IAMChannelInfoTest();
+            // t9.DoTests();
+
+            // IAMDecoderCapsTest t10 = new IAMDecoderCapsTest();
+            // t10.DoTests();
+
+            // IAMOverlayFXTest t12 = new IAMOverlayFXTest();
+            // t12.DoTests();
+
+            // IAMStreamSelectTest t13 = new IAMStreamSelectTest();
+            // t13.DoTests();
+
+            // ICodecAPITest t14 = new ICodecAPITest();
+            // t14.DoTests();
+
+            // IFilterMapper3Test t15 = new IFilterMapper3Test();
+            // t15.DoTests();
+
+            // IGetCapabilitiesKeyTest t16 = new IGetCapabilitiesKeyTest();
+            // t16.DoTests();
+
+            // IMediaSample2Test t17 = new IMediaSample2Test();
+            // t17.DoTests();
+
+            // IReferenceClock2Test t18 = new IReferenceClock2Test();
+            // t18.DoTests();
+
+            // IAMCertifiedOutputProtectionTest t19 = new IAMCertifiedOutputProtectionTest();
+            // t19.DoTests();
+
+            // IAMClockSlaveTest t20 = new IAMClockSlaveTest();
+            // t20.DoTests();
+
+            // IAMExtDeviceTest t21 = new IAMExtDeviceTest();
+            // t21.DoTests();
+
+            // IAMExtendedSeekingTest t22 = new IAMExtendedSeekingTest();
+            // t22.DoTests();
+
+            // IAMExtTransportTest t23 = new IAMExtTransportTest();
+            // t23.DoTests();
+
+            // IAMResourceControlTest t24 = new IAMResourceControlTest();
+            // t24.DoTests();
+
+            // IAMTimecodeReaderTest t25 = new IAMTimecodeReaderTest();
+            // t25.DoTests();
+
+            // IAMVideoDecimationPropertiesTest t26 = new IAMVideoDecimationPropertiesTest();
+            // t26.DoTests();
+
+            // IAMWstDecoderTest t27 = new IAMWstDecoderTest();
+            // t27.DoTests();
+
+            // IDecimateVideoImageTest t28 = new IDecimateVideoImageTest();
+            // t28.DoTests();
+
+            // IDeferredCommandTest t29 = new IDeferredCommandTest();
+            // t29.DoTests();
+
+            // IMixerOCXTest t30 = new IMixerOCXTest();
+            // t30.DoTests();
+
+            // IMixerOCXNotifyTest t31 = new IMixerOCXNotifyTest();
+            // t31.DoTests();
+
+            // IQualityControlTest t32 = new IQualityControlTest();
+            // t32.DoTests();
+
+            // IQueueCommandTest t33 = new IQueueCommandTest();
+            // t33.DoTests();
+
+            // ISelectorTest t34 = new ISelectorTest();
+            // t34.DoTests();
+
+            // IVPManagerTest t35 = new IVPManagerTest();
+            // t35.DoTests();
+
+            // IDXEffectTest t36 = new IDXEffectTest();
+            // t36.DoTests();
+
+            // IDxtAlphaSetterTest t37 = new IDxtAlphaSetterTest();
+            // t37.DoTests();
+
+            // IDxtCompositorTest t38 = new IDxtCompositorTest();
+            // t38.DoTests();
+
+            // IDxtJpegTest t39 = new IDxtJpegTest();
+            // t39.DoTests();
+
+            // IDxtKeyTest t40 = new IDxtKeyTest();
+            // t40.DoTests();
+
+            // IDMOQualityControlTest t41 = new IDMOQualityControlTest();
+            // t41.DoTests();
+
+            // IDMOVideoOutputOptimizationsTest t42 = new IDMOVideoOutputOptimizationsTest();
+            // t42.DoTests();
+
+            // IEnumStreamIdMapTest t43 = new IEnumStreamIdMapTest();
+            // t43.DoTests();
+
+            // IMpeg2DemultiplexerTest t44 = new IMpeg2DemultiplexerTest();
+            // t44.DoTests();
+
+            // IMPEG2StreamIdMapTest t45 = new IMPEG2StreamIdMapTest();
+            // t45.DoTests();
+
+            // IMpegAudioDecoderTest t46 = new IMpegAudioDecoderTest();
+            // t46.DoTests();
+
+            // IAMMediaTypeSampleTest t47 = new IAMMediaTypeSampleTest();
+            // t47.DoTests();
+
+            // IAMMediaTypeStreamTest t48 = new IAMMediaTypeStreamTest();
+            // t48.DoTests();
+
+            // IAMMultiMediaStreamTest t49 = new IAMMultiMediaStreamTest();
+            // t49.DoTests();
+
+            // IAudioDataTest t50 = new IAudioDataTest();
+            // t50.DoTests();
+
+            // IAudioMediaStreamTest t51 = new IAudioMediaStreamTest();
+            // t51.DoTests();
+
+            // IAudioStreamSampleTest t52 = new IAudioStreamSampleTest();
+            // t52.DoTests();
+
+            // IMediaStreamTest t53 = new IMediaStreamTest();
+            // t53.DoTests();
+
+            // IMemoryDataTest t54 = new IMemoryDataTest();
+            // t54.DoTests();
+
+            // IMultiMediaStreamTest t55 = new IMultiMediaStreamTest();
+            // t55.DoTests();
+
+            // IStreamSampleTest t56 = new IStreamSampleTest();
+            // t56.DoTests();
+
+            // IBDA_AutoDemodulateTest t57 = new IBDA_AutoDemodulateTest();
+            // t57.DoTests();
+
+            // IBDA_DeviceControlTest t58 = new IBDA_DeviceControlTest();
+            // t58.DoTests();
+
+            // IBDA_DigitalDemodulatorTest t59 = new IBDA_DigitalDemodulatorTest();
+            // t59.DoTests();
+
+            // IBDA_FrequencyFilterTest t60 = new IBDA_FrequencyFilterTest();
+            // t60.DoTests();
+
+            // IBDA_LNBInfoTest t61 = new IBDA_LNBInfoTest();
+            // t61.DoTests();
+
+            // IBDA_NullTransformTest t62 = new IBDA_NullTransformTest();
+            // t62.DoTests();
+
+            // IBDA_PinControlTest t63 = new IBDA_PinControlTest();
+            // t63.DoTests();
+
+            // IBDA_VoidTransformTest t64 = new IBDA_VoidTransformTest();
+            // t64.DoTests();
+
+            // IEnumGuideDataPropertiesTest t65 = new IEnumGuideDataPropertiesTest();
+            // t65.DoTests();
+
+            // IEnumPIDMapTest t66 = new IEnumPIDMapTest();
+            // t66.DoTests();
+
+            // IEnumTuneRequestsTest t67 = new IEnumTuneRequestsTest();
+            // t67.DoTests();
+
+            // IGuideDataTest t68 = new IGuideDataTest();
+            // t68.DoTests();
+
+            // IGuideDataEventTest t69 = new IGuideDataEventTest();
+            // t69.DoTests();
+
+            // IGuideDataLoaderTest t70 = new IGuideDataLoaderTest();
+            // t70.DoTests();
+
+            // IGuideDataPropertyTest t71 = new IGuideDataPropertyTest();
+            // t71.DoTests();
+
+            // ILanguageComponentTypeTest t72 = new ILanguageComponentTypeTest();
+            // t72.DoTests();
+
+            // IMPEG2ComponentTest t73 = new IMPEG2ComponentTest();
+            // t73.DoTests();
+
+            // IMPEG2ComponentTypeTest t74 = new IMPEG2ComponentTypeTest();
+            // t74.DoTests();
+
+            // IMpeg2DataTest t75 = new IMpeg2DataTest();
+            // t75.DoTests();
+
+            // IMPEG2PIDMapTest t76 = new IMPEG2PIDMapTest();
+            // t76.DoTests();
+
+            // IMpeg2StreamTest t77 = new IMpeg2StreamTest();
+            // t77.DoTests();
+
+            // IMPEG2TuneRequestSupportTest t78 = new IMPEG2TuneRequestSupportTest();
+            // t78.DoTests();
+
+            // ISectionListTest t79 = new ISectionListTest();
+            // t79.DoTests();
+
+            // ITuneRequestInfoTest t80 = new ITuneRequestInfoTest();
+            // t80.DoTests();
+
+            // IAnalogTVTuningSpaceTest t81 = new IAnalogTVTuningSpaceTest();
+            // t81.DoTests();
+
+            // IATSCChannelTuneRequestTest t82 = new IATSCChannelTuneRequestTest();
+            // t82.DoTests();
+
+            // IATSCComponentTypeTest t83 = new IATSCComponentTypeTest();
+            // t83.DoTests();
+
+            // IATSCLocatorTest t84 = new IATSCLocatorTest();
+            // t84.DoTests();
+
+            // IATSCTuningSpaceTest t85 = new IATSCTuningSpaceTest();
+            // t85.DoTests();
+
+            // IAuxInTuningSpaceTest t86 = new IAuxInTuningSpaceTest();
+            // t86.DoTests();
+
+            // IBDA_EthernetFilterTest t87 = new IBDA_EthernetFilterTest();
+            // t87.DoTests();
+
+            // IBDA_IPSinkControlTest t88 = new IBDA_IPSinkControlTest();
+            // t88.DoTests();
+
+            // IBDA_IPSinkInfoTest t89 = new IBDA_IPSinkInfoTest();
+            // t89.DoTests();
+
+            // IBDA_IPV4FilterTest t90 = new IBDA_IPV4FilterTest();
+            // t90.DoTests();
+
+            // IBDA_IPV6FilterTest t91 = new IBDA_IPV6FilterTest();
+            // t91.DoTests();
+
+            // IBDA_NetworkProviderTest t92 = new IBDA_NetworkProviderTest();
+            // t92.DoTests();
+
+            // IChannelTuneRequestTest t93 = new IChannelTuneRequestTest();
+            // t93.DoTests();
+
+            // IDVBCLocatorTest t94 = new IDVBCLocatorTest();
+            // t94.DoTests();
+
+            // IRegisterTunerTest t95 = new IRegisterTunerTest();
+            // t95.DoTests();
+
+            // IAMVfwCaptureDialogsTest t96 = new IAMVfwCaptureDialogsTest();
+            // t96.DoTests();
+
+            // IAMMediaContent2Test t97 = new IAMMediaContent2Test();
+            // t97.DoTests();
+        }
+    }
+}
