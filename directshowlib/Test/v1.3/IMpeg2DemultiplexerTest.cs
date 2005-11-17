191ee83db8ee07f12b55cb482d066bad57f9763c
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2005-11-17 06:37:31 +0000

    Finished IMpeg2Demultiplexer
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@707 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Test/v1.3/IMpeg2DemultiplexerTest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+using DirectShowLib;
+
+namespace DirectShowLib.Test
+{
+    public class IMpeg2DemultiplexerTest
+    {
+        IMpeg2Demultiplexer m_ism;
+
+        public IMpeg2DemultiplexerTest()
+        {
+        }
+
+        public void DoTests()
+        {
+            Configure();
+
+            try
+            {
+                TestCreate();
+                TestSet();
+                TestDelete();
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(m_ism);
+            }
+        }
+
+        private void TestCreate()
+        {
+            int hr;
+            IPin pPin;
+            AMMediaType amt = new AMMediaType();
+
+            hr = m_ism.CreateOutputPin(amt, "Pin1", out pPin);
+            DsError.ThrowExceptionForHR(hr);
+        }
+
+        private void TestSet()
+        {
+            int hr;
+            AMMediaType amt = new AMMediaType();
+            amt.majorType = MediaType.Text;
+
+            hr = m_ism.SetOutputPinMediaType("Pin1", amt);
+            DsError.ThrowExceptionForHR(hr);
+        }
+
+        private void TestDelete()
+        {
+            int hr;
+
+            hr = m_ism.DeleteOutputPin("Pin1");
+            DsError.ThrowExceptionForHR(hr);
+        }
+
+        private void Configure()
+        {
+            int hr;
+            IGraphBuilder gb = (IGraphBuilder )new FilterGraph();
+            DsROTEntry rot = new DsROTEntry(gb);
+            IBaseFilter pFilter = (IBaseFilter)new MPEG2Demultiplexer();
+
+            hr = gb.AddFilter(pFilter, "mpeg demux");
+
+            m_ism = (IMpeg2Demultiplexer)pFilter;
+        }
+    }
+}
