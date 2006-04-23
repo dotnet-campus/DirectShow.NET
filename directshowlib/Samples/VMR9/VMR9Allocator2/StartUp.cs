9c4986d86f18cc40a49d64d46d65b9c2e591696b
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2006-04-23 10:52:59 +0000

    A big change to run the graph (and the form) in an MTA thread
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@854 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Samples/VMR9/VMR9Allocator2/StartUp.cs
+++ directshowlib/Samples/VMR9/VMR9Allocator2/StartUp.cs
@@ -7,29 +7,48 @@ or FITNESS FOR A PARTICULAR PURPOSE.
 
 using System;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace DirectShowLib.Sample
 {
   public class StartUp
   {
+    public static string filename;
+
     [STAThread]
     static void Main() 
     {
       OpenFileDialog openDialog = new OpenFileDialog();
+
       if (openDialog.ShowDialog() == DialogResult.OK)
       {
-        using (MainForm form = new MainForm())
-        {
-          form.Show();
-          form.InitializeGraphics();
-          form.InitVMR9(openDialog.FileName);
-          form.Focus();
+        filename = openDialog.FileName;
 
-          Application.Idle += new EventHandler(form.OnApplicationIdle);
-          Application.Run(form);
-        }
+        Thread initThread = new Thread(new ThreadStart(StartUp.ApplicationLaunch));
+#if USING_NET20
+        initThread.SetApartmentState(ApartmentState.MTA);
+#else
+        initThread.ApartmentState = ApartmentState.MTA;
+#endif
+        initThread.Start();
       }
     }
 
+    static void ApplicationLaunch()
+    {
+      Application.DoEvents();
+
+      using (MainForm form = new MainForm())
+      {
+        form.Show();
+
+        form.InitializeGraphics();
+        form.InitVMR9(StartUp.filename);
+        form.Focus();
+
+        Application.Idle += new EventHandler(form.OnApplicationIdle);
+        Application.Run(form);
+      }
+    }
   }
 }
