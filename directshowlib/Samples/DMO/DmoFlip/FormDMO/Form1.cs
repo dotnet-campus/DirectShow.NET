2f5209c50d5433fa37da0b424f45b73e4d028a63
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2006-04-24 01:13:01 +0000

    Add comment when running under 2005.
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@856 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Samples/DMO/DmoFlip/FormDMO/Form1.cs
+++ directshowlib/Samples/DMO/DmoFlip/FormDMO/Form1.cs
@@ -32,6 +32,7 @@ namespace FormDMO
 
         private IFilterGraph2 graphBuilder = null;
         private IMediaParams m_param = null;
+        private System.Windows.Forms.TextBox textBox1;
         private DsROTEntry m_rot = null;
 
         public Form1()
@@ -41,7 +42,9 @@ namespace FormDMO
 			//
 			InitializeComponent();
             BuildGraph(this.panel1);
-
+#if !USING_NET11
+            textBox1.Text = "Running this code under the debugger in vs 2005 will give VERY poor performance.  Use \"Debug/Start without Debugging\" or use the unsafe code from the readme for decent performance.";
+#endif
 			//
 			// TODO: Add any constructor code after InitializeComponent call
 			//
@@ -84,12 +87,13 @@ namespace FormDMO
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(8, 232);
+            this.button1.Location = new System.Drawing.Point(8, 294);
             this.button1.Name = "button1";
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
@@ -97,8 +101,7 @@ namespace FormDMO
             // 
             // panel1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-                | System.Windows.Forms.AnchorStyles.Left) 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
                 | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -107,7 +110,8 @@ namespace FormDMO
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(104, 232);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(104, 294);
             this.button2.Name = "button2";
             this.button2.TabIndex = 2;
             this.button2.Text = "FlipX";
@@ -115,16 +119,28 @@ namespace FormDMO
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(208, 232);
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(208, 294);
             this.button3.Name = "button3";
             this.button3.TabIndex = 3;
             this.button3.Text = "FlipY";
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(8, 224);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 64);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(292, 262);
+            this.ClientSize = new System.Drawing.Size(292, 324);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
