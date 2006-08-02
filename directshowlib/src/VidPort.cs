ce205d1c9103fe3cf81423de2368547bbb66dd38
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2006-08-02 10:05:35 +0000

    put IVPBaseConfig back in untested since IVPBaseConfig can't be tested
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@922 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/src/VidPort.cs
+++ directshowlib/src/VidPort.cs
@@ -31,7 +31,7 @@ namespace DirectShowLib
     #region Declarations
 
 #if ALLOW_UNTESTED_INTERFACES
-
+    
     /// <summary>
     /// From AMVPSIZE
     /// </summary>
@@ -49,9 +49,9 @@ namespace DirectShowLib
     public struct DDVideoPortConnect
     {
         public int dwSize;
-        public int  dwPortWidth;
-        public Guid  guidTypeID;
-        public int  dwFlags;
+        public int dwPortWidth;
+        public Guid guidTypeID;
+        public int dwFlags;
         public IntPtr dwReserved1;
     }
 
@@ -61,19 +61,19 @@ namespace DirectShowLib
     [StructLayout(LayoutKind.Sequential)]
     public struct VPDataInfo
     {
-        public int           dwSize;
-        public int           dwMicrosecondsPerField;
-        public AMVPDimInfo     amvpDimInfo;
-        public int           dwPictAspectRatioX;
-        public int           dwPictAspectRatioY;
-        public bool            bEnableDoubleClock;
-        public bool            bEnableVACT;
-        public bool            bDataIsInterlaced;
-        public int            lHalfLinesOdd;
-        public bool            bFieldPolarityInverted;
-        public int           dwNumLinesInVREF;
-        public int            lHalfLinesEven;
-        public int           dwReserved1;
+        public int dwSize;
+        public int dwMicrosecondsPerField;
+        public AMVPDimInfo amvpDimInfo;
+        public int dwPictAspectRatioX;
+        public int dwPictAspectRatioY;
+        public bool bEnableDoubleClock;
+        public bool bEnableVACT;
+        public bool bDataIsInterlaced;
+        public int lHalfLinesOdd;
+        public bool bFieldPolarityInverted;
+        public int dwNumLinesInVREF;
+        public int lHalfLinesEven;
+        public int dwReserved1;
     }
 
     /// <summary>
@@ -82,13 +82,12 @@ namespace DirectShowLib
     [StructLayout(LayoutKind.Sequential)]
     public struct AMVPDimInfo
     {
-        public int           dwFieldWidth;
-        public int           dwFieldHeight;
-        public int           dwVBIWidth;
-        public int           dwVBIHeight;
-        public Rectangle            rcValidRegion;
+        public int dwFieldWidth;
+        public int dwFieldHeight;
+        public int dwVBIWidth;
+        public int dwVBIHeight;
+        public Rectangle rcValidRegion;
     }
-
 
 #endif
 
@@ -186,177 +185,11 @@ namespace DirectShowLib
     }
 
     [ComImport,
-    Guid("EC529B00-1A1F-11D1-BAD9-00609744111A"),
-    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IVPVBIConfig : IVPBaseConfig
-    {
-        #region IVPBaseConfig Methods
-
-        [PreserveSig]
-        new int GetConnectInfo(
-            out int pdwNumConnectInfo,
-            out DDVideoPortConnect pddVPConnectInfo
-            );
-
-        [PreserveSig]
-        new int SetConnectInfo(
-            int dwChosenEntry
-            );
-
-        [PreserveSig]
-        new int GetVPDataInfo(
-            out VPDataInfo pamvpDataInfo
-            );
-
-        [PreserveSig]
-        new int GetMaxPixelRate(
-            out AMVPSize pamvpSize,
-            out int pdwMaxPixelsPerSecond
-            );
-
-        [PreserveSig]
-        new int InformVPInputFormats(
-            int dwNumFormats,
-            DDPixelFormat pDDPixelFormats
-            );
-
-        [PreserveSig]
-        new int GetVideoFormats(
-            out int pdwNumFormats,
-            out DDPixelFormat pddPixelFormats
-            );
-
-        [PreserveSig]
-        new int SetVideoFormat(
-            int dwChosenEntry
-            );
-
-        [PreserveSig]
-        new int SetInvertPolarity(
-            );
-
-        [PreserveSig]
-        new int GetOverlaySurface(
-            out IntPtr ppddOverlaySurface // IDirectDrawSurface
-            );
-
-        [PreserveSig]
-        new int SetDirectDrawKernelHandle(
-            IntPtr dwDDKernelHandle
-            );
-
-        [PreserveSig]
-        new int SetVideoPortID(
-            int dwVideoPortID
-            );
-
-        [PreserveSig]
-        new int SetDDSurfaceKernelHandles(
-            int cHandles,
-            IntPtr rgDDKernelHandles
-            );
-
-        [PreserveSig]
-        new int SetSurfaceParameters(
-            int dwPitch,
-            int dwXOrigin,
-            int dwYOrigin
-            );
-
-        #endregion
-
-    }
-
-    [ComImport,
-    Guid("EC529B01-1A1F-11D1-BAD9-00609744111A"),
-    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IVPVBINotify : IVPBaseNotify
-    {
-        #region IVPBaseNotify
-
-        [PreserveSig]
-        new int RenegotiateVPParameters();
-
-        #endregion
-
-    }
-
-#endif
-
-    [ComImport,
-    Guid("C76794A1-D6C5-11d0-9E69-00C04FD7C15B"),
-    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IVPNotify : IVPBaseNotify
-    {
-        #region IVPBaseNotify
-
-        [PreserveSig]
-        new int RenegotiateVPParameters();
-
-        #endregion
-
-        [PreserveSig]
-        int SetDeinterlaceMode(
-            AMVP_Mode mode
-            );
-
-        [PreserveSig]
-        int GetDeinterlaceMode(
-            out AMVP_Mode pMode
-            );
-    }
-
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IVPBaseNotify
-    {
-        [PreserveSig]
-        int RenegotiateVPParameters();
-    }
-
-    [ComImport,
-    Guid("EBF47183-8764-11d1-9E69-00C04FD7C15B"),
-    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    public interface IVPNotify2 : IVPNotify
-    {
-        #region IVPBaseNotify
-
-        [PreserveSig]
-        new int RenegotiateVPParameters();
-
-        #endregion
-
-        #region IVPNotify Methods
-
-        [PreserveSig]
-        new int SetDeinterlaceMode(
-            AMVP_Mode mode
-            );
-
-        [PreserveSig]
-        new int GetDeinterlaceMode(
-            out AMVP_Mode pMode
-            );
-
-        #endregion
-
-        [PreserveSig]
-        int SetVPSyncMaster(
-            [MarshalAs(UnmanagedType.Bool)] bool bVPSyncMaster
-            );
-
-        [PreserveSig]
-        int GetVPSyncMaster(
-            [MarshalAs(UnmanagedType.Bool)] out bool pbVPSyncMaster
-            );
-
-    }
-
-    [ComImport,
     Guid("BC29A660-30E3-11d0-9E69-00C04FD7C15B"),
     InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IVPConfig : IVPBaseConfig
     {
-        #region IVPBaseConfig Methods
+    #region IVPBaseConfig Methods
 
         [PreserveSig]
         new int GetConnectInfo(
@@ -440,6 +273,172 @@ namespace DirectShowLib
         int SetScalingFactors(
             AMVPSize pamvpSize
             );
+    }
+
+    [ComImport,
+    Guid("EC529B00-1A1F-11D1-BAD9-00609744111A"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVPVBIConfig : IVPBaseConfig
+    {
+    #region IVPBaseConfig Methods
+
+        [PreserveSig]
+        new int GetConnectInfo(
+            out int pdwNumConnectInfo,
+            out DDVideoPortConnect pddVPConnectInfo
+            );
+
+        [PreserveSig]
+        new int SetConnectInfo(
+            int dwChosenEntry
+            );
+
+        [PreserveSig]
+        new int GetVPDataInfo(
+            out VPDataInfo pamvpDataInfo
+            );
+
+        [PreserveSig]
+        new int GetMaxPixelRate(
+            out AMVPSize pamvpSize,
+            out int pdwMaxPixelsPerSecond
+            );
+
+        [PreserveSig]
+        new int InformVPInputFormats(
+            int dwNumFormats,
+            DDPixelFormat pDDPixelFormats
+            );
+
+        [PreserveSig]
+        new int GetVideoFormats(
+            out int pdwNumFormats,
+            out DDPixelFormat pddPixelFormats
+            );
+
+        [PreserveSig]
+        new int SetVideoFormat(
+            int dwChosenEntry
+            );
+
+        [PreserveSig]
+        new int SetInvertPolarity(
+            );
+
+        [PreserveSig]
+        new int GetOverlaySurface(
+            out IntPtr ppddOverlaySurface // IDirectDrawSurface
+            );
+
+        [PreserveSig]
+        new int SetDirectDrawKernelHandle(
+            IntPtr dwDDKernelHandle
+            );
+
+        [PreserveSig]
+        new int SetVideoPortID(
+            int dwVideoPortID
+            );
+
+        [PreserveSig]
+        new int SetDDSurfaceKernelHandles(
+            int cHandles,
+            IntPtr rgDDKernelHandles
+            );
+
+        [PreserveSig]
+        new int SetSurfaceParameters(
+            int dwPitch,
+            int dwXOrigin,
+            int dwYOrigin
+            );
+
+        #endregion
+
+    }
+
+    [ComImport,
+    Guid("EC529B01-1A1F-11D1-BAD9-00609744111A"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVPVBINotify : IVPBaseNotify
+    {
+    #region IVPBaseNotify
+
+        [PreserveSig]
+        new int RenegotiateVPParameters();
+
+        #endregion
+
+    }
+
+#endif
+
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVPBaseNotify
+    {
+        [PreserveSig]
+        int RenegotiateVPParameters();
+    }
+
+    [ComImport,
+    Guid("C76794A1-D6C5-11d0-9E69-00C04FD7C15B"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVPNotify : IVPBaseNotify
+    {
+        #region IVPBaseNotify
+
+        [PreserveSig]
+        new int RenegotiateVPParameters();
+
+        #endregion
+
+        [PreserveSig]
+        int SetDeinterlaceMode(
+            AMVP_Mode mode
+            );
+
+        [PreserveSig]
+        int GetDeinterlaceMode(
+            out AMVP_Mode pMode
+            );
+    }
+
+    [ComImport,
+    Guid("EBF47183-8764-11d1-9E69-00C04FD7C15B"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IVPNotify2 : IVPNotify
+    {
+        #region IVPBaseNotify
+
+        [PreserveSig]
+        new int RenegotiateVPParameters();
+
+        #endregion
+
+        #region IVPNotify Methods
+
+        [PreserveSig]
+        new int SetDeinterlaceMode(
+            AMVP_Mode mode
+            );
+
+        [PreserveSig]
+        new int GetDeinterlaceMode(
+            out AMVP_Mode pMode
+            );
+
+        #endregion
+
+        [PreserveSig]
+        int SetVPSyncMaster(
+            [MarshalAs(UnmanagedType.Bool)] bool bVPSyncMaster
+            );
+
+        [PreserveSig]
+        int GetVPSyncMaster(
+            [MarshalAs(UnmanagedType.Bool)] out bool pbVPSyncMaster
+            );
+
     }
 
     #endregion
