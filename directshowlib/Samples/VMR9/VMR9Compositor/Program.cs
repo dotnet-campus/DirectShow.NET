afb00d8f833af5a4c67a57c96560e4187ed52056
nowinskie <nowinskie@b2a09c84-0b36-484d-b36e-ffab392acda3>
2006-08-08 11:42:00 +0000

    The VMR9Compositor Sample
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@927 b2a09c84-0b36-484d-b36e-ffab392acda3

---
+++ directshowlib/Samples/VMR9/VMR9Compositor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DirectShowLib.Sample
+{
+  static class Program
+  {
+    /// <summary>
+    /// Point d'entrée principal de l'application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      using (MainForm form = new MainForm())
+      {
+        Application.Run(form);
+      }
+    }
+  }
+}
