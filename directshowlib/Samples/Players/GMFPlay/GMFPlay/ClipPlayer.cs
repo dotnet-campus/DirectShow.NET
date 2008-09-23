01df40d840be499c3925ac23b4f52100a0d6fe52
snarfle <snarfle@b2a09c84-0b36-484d-b36e-ffab392acda3>
2008-09-23 06:37:51 +0000

    Add copyright notices
    git-svn-id: https://svn.code.sf.net/p/directshownet/code/trunk@1090 b2a09c84-0b36-484d-b36e-ffab392acda3

--- directshowlib/Samples/Players/GMFPlay/GMFPlay/ClipPlayer.cs
+++ directshowlib/Samples/Players/GMFPlay/GMFPlay/ClipPlayer.cs
@@ -1,3 +1,10 @@
+/****************************************************************************
+While the underlying libraries are covered by LGPL, this sample is released 
+as public domain.  It is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE.  
+*****************************************************************************/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
