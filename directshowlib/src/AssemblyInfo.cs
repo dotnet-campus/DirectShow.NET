67b1303393e4d41a1eac2cebdab74f43425d9236
Shaojun Li <i@shaojun.li>
2020-07-30 16:36:48 +0800

    SDK sytle csproj, support net40 and netcoreapp31, remove useless things.

--- directshowlib/src/AssemblyInfo.cs
+++ directshowlib/src/AssemblyInfo.cs
@@ -23,31 +23,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endregion
 
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
-
-[assembly : AssemblyTitle("DirectShow Net Library")]
-[assembly : AssemblyDescription(".NET Interfaces for calling DirectShow.  See http://directshownet.sourceforge.net/")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : Guid("6D0386CE-37E6-4f77-B678-07C584105DC6")]
-[assembly : AssemblyVersion("2.1.0.*")]
-#if DEBUG
-[assembly : AssemblyProduct("Debug Version")]
-#else
-[assembly : AssemblyProduct("Release Version")]
-#endif
-[assembly : AssemblyCopyright("GNU Lesser General Public License v2.1")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
-[assembly : AssemblyDelaySign(false)]
-// Path is relative to the resulting executable (\Bin\Debug)
-#if USING_NET11
-[assembly : AssemblyKeyFile("..\\..\\DShowNET.snk")]
-#endif
-[assembly : AssemblyKeyName("")]
 [assembly : ComVisible(false)]
 [assembly : CLSCompliant(true)]
-[assembly : SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode=true)]
